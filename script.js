7f862ccdbb54f3dca7d993ec1d1ee840b8469ef6
Rein Marion <reincoloquio@gmail.com>
2020-01-25 23:56:39 -0500

    add arrays for timeblock

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//creating the time in header 
 var timeBlocks = $(".container");
 var timeDisplay = moment().format('dddd, MMMM Do YYYY');
 
 var timeDiv = $("#currentDay");
 timeDiv.append(timeDisplay);
 
+// arrays for timeblock 
+var timeList = ["9 AM", "10 AM", "11 AM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM",]
+var idTime = ["9", "10", "11", "12", "13", "14", "15", "16", "17",] 
